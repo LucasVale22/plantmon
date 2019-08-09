51604c92f076ffecb09946a4a5e1123e507652c9
LucasVale22 <lukasvale22@gmail.com>
2019-08-09 11:57:47 -0300

    Atualizado

--- websocket_test_MVC/server/app.js
+++ websocket_test_MVC/server/app.js
@@ -1,3 +1,21 @@
+/*importando as configurações do servidor*/
 var app = require('./config/server');
+var http = require('http');
+var path = require('path');
+var address;
 
-const server = http.createServer(app);//create a serverr
+/*criando um servidor*/
+const server = http.createServer(app);//create a server
+
+/*regastando o ip local do servidor node.js (este ip deve estar do lado do cliente)*/
+require('dns').lookup(require('os').hostname(), function (err, add, fam) {
+    console.log('addr: '+add);
+    address = add;
+});
+
+/*parametrizando a porta de escuta*/
+server.listen(3000, function(){
+    console.log('**---> SERVIDOR ONLINE <---**');
+});
+
+module.exports = address;
